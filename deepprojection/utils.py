eba5157a90620afe2f488869a1c879ee4c7363f1
carbonscott <wangimagine@gmail.com>
2022-02-03 20:41:13 -0800

    Support analyzing log files.

--- deepprojection/utils.py
+++ deepprojection/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import skimage.measure as sm
 
 def downsample(assem, bin_row=2, bin_col=2, mask=None):
-    """ Helper function to img_to_thumbnail(). 
+    """ Downsample an SPI image.  
         Adopted from https://github.com/chuckie82/DeepProjection/blob/master/DeepProjection/utils.py
     """
     if mask is None:
@@ -16,3 +16,32 @@ def downsample(assem, bin_row=2, bin_col=2, mask=None):
     warr[ind]  = downCalib[ind] / downWeight[ind]
 
     return warr
+
+
+def read_log(file):
+    '''Return all lines in the user supplied parameter file without comments.
+    '''
+    # Retrieve key-value information...
+    kw_kv     = "KV - "
+    kv_dict   = {}
+
+    # Retrieve data information...
+    kw_data   = "DATA - "
+    data_dict = {}
+    with open(file,'r') as fh:
+        for line in fh.readlines():
+            # Collect kv information...
+            if kw_kv in line:
+                info = line[line.rfind(kw_kv) + len(kw_kv):]
+                k, v = info.split(":")
+                if not k in kv_dict: kv_dict[k.strip()] = v.strip()
+
+            # Collect data information...
+            if kw_data in line:
+                info = line[line.rfind(kw_data) + len(kw_data):]
+                k = tuple( info.strip().split(",") )
+                if not k in data_dict: data_dict[k] = True
+
+    ret_dict = { "kv" : kv_dict, "data" : tuple(data_dict.keys()) }
+
+    return ret_dict
---
+++ examples/alog.train.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+from deepprojection.utils import read_log
+
+logging.basicConfig( format="%(asctime)s %(levelname)s %(name)-10s - %(message)s",
+                     datefmt="%m/%d/%Y %H:%M:%S",
+                     level=logging.INFO, )
+logger = logging.getLogger(__name__)
+
+# Locate the path of the log file...
+id_log   = '20220203114714'
+fl_log   = f'{id_log}.train.log'
+drc_log  = 'logs'
+path_log = os.path.join(drc_log, fl_log)
+
+# Read the file...
+log_dict = read_log(path_log)
+
+# Understand dataset...
+uniq_dict = {}
+comb_dict = {}
+for anchor, pos, neg in log_dict[ "data" ]:
+    anchor   = tuple(anchor.split())
+    pos      = tuple(pos.split())
+    neg      = tuple(neg.split())
+
+    for i in (anchor, pos, neg):
+        if not i in uniq_dict: uniq_dict[i] = True
+
+    comb_str = f"{anchor[-1]}{pos[-1]}{neg[-1]}"
+    if not comb_str in comb_dict: comb_dict[comb_str]  = 1
+    else                        : comb_dict[comb_str] += 1
+
+# Original datasets
+data_dict = {}
+for k in uniq_dict.keys():
+    label = k[-1]
+    if not label in data_dict: data_dict[label]  = 1
+    else                     : data_dict[label] += 1
+logger.info("___/ Tally Labels \___")
+for k, v in data_dict.items(): logger.info(f"{k} : {v}")
+
+
+# Tally combinations...
+logger.info("___/ Tally Combination \___")
+for k, v in comb_dict.items(): logger.info(f"{k} : {v}")
