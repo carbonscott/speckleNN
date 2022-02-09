578189bb1bd6aee0731af35e0df80e646894fe0c
carbonscott <wangimagine@gmail.com>
2022-02-08 22:59:47 -0800

    Bookkeeping

--- examples/explore.normalize.py
+++ examples/explore.normalize.py
@@ -3,6 +3,9 @@
 
 import psana
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import matplotlib.patches as mpatches
+import matplotlib.transforms as mtransforms
 import numpy as np
 
 
@@ -59,16 +62,16 @@ class HistSPIImg():
         plt.rcParams.update({'font.size': 18})
         plt.rcParams.update({'font.family' : 'sans-serif'})
 
-        nrows, ncols = 1, 2
+        nrows, ncols = 3, 2
         fig = plt.figure(figsize = self.figsize)
 
         gspec   = fig.add_gridspec(nrows, ncols)
-        ax_img  = fig.add_subplot(gspec[0,0])
-        ax_norm = fig.add_subplot(gspec[0,1])
+        ax_img  = (fig.add_subplot(gspec[0,0]), fig.add_subplot(gspec[1:,0]))
+        ax_norm = (fig.add_subplot(gspec[0,1]), fig.add_subplot(gspec[1:,1]))
         return fig, (ax_img, ax_norm)
 
 
-    def plot_img(self, rng = [], bin_cap = 10000): 
+    def plot_img(self, rng = [], bin_cap = 10000, vcenter = 0, vmin = 0, vmax = 1): 
         img = self.img
         bin_val, bin_rng = self.population_density(img, bin_cap)
 
@@ -78,22 +81,44 @@ class HistSPIImg():
         if len(rng) == 0: rng = bin_rng[0], bin_rng[-1]
         data = []
         for i in range(len(bin_val)):
-            data.append([bin_rng[i], bin_val[i]])
+            data.append([bin_rng[i],   bin_val[i]])
             data.append([bin_rng[i+1], bin_val[i]])
         data = np.array(data)
-        self.ax_img.plot(data[:, 0], data[:, 1])
-        self.ax_img.set_yscale('log')
-        self.ax_img.set_xlim(rng[0], rng[1])
-        self.ax_img.set_ylim(min(bin_val), max(bin_val))
-        self.ax_img.set_title(f"$\mu = {img_mean:.4f}$; $\sigma = {img_std:.4f};  b = {bin_cap}$")
-        self.ax_img.set_xlabel('Pixel intensity')
-        self.ax_img.set_ylabel('Population density')
+
+        # Plot population density...
+        self.ax_img[0].plot(data[:, 0], data[:, 1])
+        self.ax_img[0].set_box_aspect(0.5)
+        self.ax_img[0].set_yscale('log')
+        self.ax_img[0].set_xlim(rng[0], rng[1])
+        ymin, ymax = min(bin_val), max(bin_val)
+        self.ax_img[0].set_ylim(ymin, ymax)
+        self.ax_img[0].set_title(f"$\mu = {img_mean:.4f}$; $\sigma = {img_std:.4f};  b = {bin_cap}$")
+        self.ax_img[0].set_xlabel('Pixel intensity')
+        self.ax_img[0].set_ylabel('Population density')
         print(f"Image: ")
         print(f"Range X: [{bin_rng[0]}, {bin_rng[-1]}]")
         print(f"Range Y: [{bin_val[0]}, {bin_val[-1]}]")
 
+        # Add a rectangle to specify the range (vmin, vmax)...
+        mtrans = mtransforms.blended_transform_factory(self.ax_img[0].transData, self.ax_img[0].transAxes)
+        #                                                  ~~~~~~~~~~~~  ~~~~~~~~~~~~
+        # Axes X (Keep as DC) ___________________________________|             :
+        #                                                                      :
+        # Axes Y (Transform to AC) ............................................:
 
-    def plot_norm(self, rng = [], bin_cap = 10000): 
+        w = vmax - vmin
+        rect = mpatches.Rectangle((vmin, 0), width=w, height=1, transform=mtrans,
+                                  color='yellow', alpha=0.5)
+        if not vcenter == None: self.ax_img[0].add_patch(rect)
+
+        # Plot image...
+        divnorm = mcolors.TwoSlopeNorm(vcenter = vcenter, vmin = vmin, vmax = vmax)
+        im = self.ax_img[1].imshow(img, norm = divnorm)
+        im.set_cmap('seismic')
+        plt.colorbar(im, ax = self.ax_img[1],orientation="horizontal", pad = 0.05)
+
+
+    def plot_norm(self, rng = [], bin_cap = 10000, vcenter = 0, vmin = 0, vmax = 1): 
         img = self.img
         img_mean = self.img_mean
         img_std  = self.img_std
@@ -106,24 +131,42 @@ class HistSPIImg():
         if len(rng) == 0: rng = bin_rng[0], bin_rng[-1]
         data = []
         for i in range(len(bin_val)):
-            data.append([bin_rng[i], bin_val[i]])
+            data.append([bin_rng[i],   bin_val[i]])
             data.append([bin_rng[i+1], bin_val[i]])
         data = np.array(data)
-        self.ax_norm.plot(data[:, 0], data[:, 1])
-        self.ax_norm.set_yscale('log')
-        self.ax_norm.set_xlim(rng[0], rng[1])
-        self.ax_norm.set_ylim(min(bin_val), max(bin_val))
-        self.ax_norm.set_title(f"$\mu = {img_mean:.4f}$; $\sigma = {img_std:.4f};  b = {bin_cap}$")
-        self.ax_norm.set_xlabel('Pixel intensity')
-        self.ax_norm.set_ylabel('Population density')
+
+        # Plot population density...
+        self.ax_norm[0].plot(data[:, 0], data[:, 1])
+        self.ax_norm[0].set_box_aspect(0.5)
+        self.ax_norm[0].set_yscale('log')
+        self.ax_norm[0].set_xlim(rng[0], rng[1])
+        ymin, ymax = min(bin_val), max(bin_val)
+        self.ax_norm[0].set_ylim(ymin, ymax)
+        self.ax_norm[0].set_title(f"$\mu = {img_mean:.4f}$; $\sigma = {img_std:.4f};  b = {bin_cap}$")
+        self.ax_norm[0].set_xlabel('Pixel intensity')
+        self.ax_norm[0].set_ylabel('Population density')
         print(f"Norm: ")
         print(f"Range X: [{bin_rng[0]}, {bin_rng[-1]}]")
         print(f"Range Y: [{bin_val[0]}, {bin_val[-1]}]")
 
+        # Add a rectangle to specify the range (vmin, vmax)...
+        mtrans = mtransforms.blended_transform_factory(self.ax_norm[0].transData, self.ax_norm[0].transAxes)
+
+        # Add a rectangle to specify the range (vmin, vmax)...
+        w = vmax - vmin
+        rect = mpatches.Rectangle((vmin, 0), width=w, height=1, transform=mtrans,
+                                  color='yellow', alpha=0.5)
+        if not vcenter == None: self.ax_norm[0].add_patch(rect)
+
+        # Plot image...
+        divnorm = mcolors.TwoSlopeNorm(vcenter = vcenter, vmin = vmin, vmax = vmax)
+        im = self.ax_norm[1].imshow(img_norm, norm = divnorm)
+        im.set_cmap('seismic')
+        plt.colorbar(im, ax = self.ax_norm[1], orientation="horizontal", pad = 0.05)
 
     def show(self): 
-        self.plot_img (rng = [], bin_cap = 200)
-        self.plot_norm(rng = [], bin_cap = 200)
+        self.plot_img (rng = [-150, 250], bin_cap = 200, vcenter = 0, vmin = -120,  vmax = 150)
+        self.plot_norm(rng = [-1,     5], bin_cap = 200, vcenter = 0, vmin = -1,    vmax = 1)
 
         img_mean = self.img_mean
         img_std  = self.img_std
@@ -172,10 +215,12 @@ exp, run, mode, detector_name = 'amo06516', '90', 'idx', 'pnccdFront'
 img_reader = PsanaImg(exp, run, mode, detector_name)
 
 # Access an image (e.g. event 796)...
-event_num = 796
+## event_num = 796
+## event_num = 1997
+event_num = 3120
 img = img_reader.get(event_num, mode = "image")
 ## img_raw = img_reader.get(event_num, mode = "raw")
 
 # Dispaly an image...
-disp_manager = HistSPIImg(img, figsize = (18, 8))
+disp_manager = HistSPIImg(img, figsize = (18, 18))
 disp_manager.show()
