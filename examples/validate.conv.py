a7b5709c2559bdd9ccc33f75ece75ff4ad4297be
carbonscott <wangimagine@gmail.com>
2022-02-26 21:07:44 -0800

    Update validation example

--- examples/validate.conv.py
+++ examples/validate.conv.py
@@ -8,20 +8,33 @@ import torch
 from deepprojection.datasets.experiments import SPIImgDataset      , SiameseTestset, ConfigDataset
 from deepprojection.model                import SiameseModelCompare, ConfigSiameseModel
 from deepprojection.validator            import PairValidator      , ConfigValidator
-from deepprojection.encoders.linear      import SimpleEncoder      , SimpleEncoder2,  ConfigEncoder
+from deepprojection.encoders.convnet     import Hirotaka0122       , ConfigEncoder
+from deepprojection.datasets             import transform
 
 
 # Create a timestamp to name the log file...
-## timestamp = "20220203150247"
-## timestamp = "20220203115233"
-## timestamp = "20220214122748"
-timestamp = "20220215220550"
+## timestamp = "20220225214556"    # size_sample : 500, alpha : 2.6
+## timestamp = "20220225214804"    # 2000, 2.6
+## timestamp = "20220225214825"    # 2000, 2.0
+## timestamp = "20220225235026"    # 1500, 2.6
+## timestamp = "20220225234941"    # 1500, 2.0
+## timestamp = "20220225235155"    # 1500, 2.2
+## timestamp = "20220225235242"    # 1500, 1.6
+## timestamp = "20220225235121"    # 1500, 2.4
+## timestamp = "20220225234917"    # 2000, 1.6
+## timestamp = "20220225234902"    # 2000, 2.4
+timestamp = "20220225234851"    # 2000, 2.2
+## timestamp = "20220225234840"    # 2000, 1.8
+
+# Validate mode...
+istrain = True
+mode_validate = 'train' if istrain else 'test'
 
 # Configure the location to run the job...
 drc_cwd = os.getcwd()
 
 # Set up the log file...
-fl_log         = f"{timestamp}.validate.pair.log"
+fl_log         = f"{timestamp}.validate.{mode_validate}.log"
 DRCLOG         = "logs"
 prefixpath_log = os.path.join(drc_cwd, DRCLOG)
 if not os.path.exists(prefixpath_log): os.makedirs(prefixpath_log)
@@ -42,7 +55,11 @@ config_dataset = ConfigDataset( fl_csv         = 'datasets.csv',
                                 mode           = 'image',
                                 mask           = None,
                                 resize         = None,
-                                isflat         = True,
+                                seed           = 0,
+                                isflat         = False,
+                                istrain        = istrain,
+                                trans          = None,
+                                frac_train     = 0.8,
                                 exclude_labels = exclude_labels, )
 
 # Get image size...
@@ -64,11 +81,26 @@ mask[mask_false_area[0], mask_false_area[1]] = 0
 mask_false_area = (slice(510, None), slice(541, 670))
 mask[mask_false_area[0], mask_false_area[1]] = 0
 
+# Random transformation for data augmentation...
+# Random rotation
+angle = None
+center = (524, 506)
+trans_random_rotate = transform.RandomRotate(angle = angle, center = center)
+
+# Random patching
+num_patch = 5
+size_patch_y, size_patch_x = 70, 500
+trans_random_patch  = transform.RandomPatch(num_patch             , size_patch_y     , size_patch_x, 
+                                            var_patch_y    = 0.2  , var_patch_x    = 0.2, 
+                                            is_return_mask = False, is_random_flip = True)
+trans_list = [trans_random_rotate, trans_random_patch]
+config_dataset.trans = trans_list
+
 # Reconfig the dataset...
 resize_y, resize_x = 6, 6
 resize = (resize_y, resize_x) if not None in (resize_y, resize_x) else ()
 config_dataset.resize = resize
-config_dataset.mask   = mask
+config_dataset.mask = mask
 config_dataset.report()
 dataset_validate = SiameseTestset(config_dataset)
 
@@ -77,22 +109,19 @@ spiimg = SPIImgDataset(config_dataset)
 img    = spiimg.get_img_and_label(0)[0]
 size_y, size_x = img.shape
 
-# Set up the checkpoint path...
 DRCCHKPT = "chkpts"
 prefixpath_chkpt = os.path.join(drc_cwd, DRCCHKPT)
 
 # Config the encoder...
-dim_emb = 32
+dim_emb = 128
 config_encoder = ConfigEncoder( dim_emb = dim_emb,
                                 size_y  = size_y,
                                 size_x  = size_x,
                                 isbias  = True )
-## encoder = SimpleEncoder(config_encoder)
-encoder = SimpleEncoder2(config_encoder)
+encoder = Hirotaka0122(config_encoder)
 
 # Set up the model
-alpha = 1.0
-config_siamese = ConfigSiameseModel( alpha = alpha, encoder = encoder, )
+config_siamese = ConfigSiameseModel( encoder = encoder, )
 model = SiameseModelCompare(config_siamese)
 
 # Read chkpt from a trainig
@@ -100,11 +129,12 @@ fl_chkpt = f"{timestamp}.train.chkpt"
 path_chkpt = os.path.join(prefixpath_chkpt, fl_chkpt)
 config_validator = ConfigValidator( path_chkpt  = path_chkpt,
                                     num_workers = 1,
-                                    batch_size  = 200,
-                                    isflat      = True,
+                                    batch_size  = 40,
+                                    pin_memory  = True,
+                                    shuffle     = False,
+                                    isflat      = False,
                                     max_epochs  = 1,    # Epoch = 1 for validate
-                                    lr          = 0.001, )
-
+                                    lr          = 1e-3, )
 
 validator = PairValidator(model, dataset_validate, config_validator)
 validator.validate()
