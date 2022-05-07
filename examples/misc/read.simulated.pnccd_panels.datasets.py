635783d783faff2974914120822c3d5f405be0bc
carbonscott <wangimagine@gmail.com>
2022-05-06 19:04:08 -0700

    Support training on pnccd images.

---
+++ examples/misc/read.simulated.pnccd_panels.datasets.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib              as mpl
+import matplotlib.pyplot       as plt
+import matplotlib.colors       as mcolors
+import matplotlib.patches      as mpatches
+import matplotlib.transforms   as mtransforms
+import matplotlib.font_manager as font_manager
+import numpy as np
+import os
+from deepprojection.datasets.simulated_panels import SPIPanelDataset, ConfigDataset
+from simulated_pnccd_panel_preprocess import DatasetPreprocess
+
+class DisplaySPIImg:
+
+    def __init__(self, img, mask, figsize, **kwargs):
+        self.img = img
+        self.mask = mask
+        self.figsize = figsize
+        for k, v in kwargs.items(): setattr(self, k, v)
+
+        self.config_fonts()
+        self.config_colorbar()
+
+    def config_fonts(self):
+        # Where to load external font...
+        drc_py    = os.path.dirname(os.path.realpath(__file__))
+        drc_font  = os.path.join("fonts", "Helvetica")
+        fl_ttf    = f"Helvetica.ttf"
+        path_font = os.path.join(drc_py, drc_font, fl_ttf)
+        prop_font = font_manager.FontProperties( fname = path_font )
+
+        # Add Font and configure font properties
+        font_manager.fontManager.addfont(path_font)
+        prop_font = font_manager.FontProperties(fname = path_font)
+        self.prop_font = prop_font
+
+        # Specify fonts for pyplot...
+        plt.rcParams['font.family'] = prop_font.get_name()
+        plt.rcParams['font.size']   = 18
+
+        return None
+
+
+    def create_panels(self):
+        nrows, ncols = 2, 2
+        fig = plt.figure(figsize = self.figsize)
+        gspec =  fig.add_gridspec(nrows, ncols, height_ratios = [1, 1/20])
+        ax_img  = fig.add_subplot(gspec[0,0], aspect = 1)
+        ax_mask = fig.add_subplot(gspec[0,1], aspect = 1)
+        ax_bar_img  = fig.add_subplot(gspec[1,0], aspect = 1/20)
+        ax_bar_mask = fig.add_subplot(gspec[1,1], aspect = 1/20)
+
+        return fig, (ax_img, ax_mask, ax_bar_img, ax_bar_mask)
+
+
+    def plot_img(self): 
+        img = self.img
+        im = self.ax_img.imshow(img, norm = self.divnorm)
+        im.set_cmap('seismic')
+        plt.colorbar(im, cax = self.ax_bar_img, orientation="horizontal", pad = 0.05)
+
+
+    def plot_mask(self): 
+        mask = self.mask
+        im = self.ax_mask.imshow(mask, norm = self.divnorm)
+        im.set_cmap('seismic')
+        plt.colorbar(im, cax = self.ax_bar_mask, orientation="horizontal", pad = 0.05)
+
+
+    def plot_center(self, center = (0, 0), angle = 0):
+        x, y = center
+        marker_obj = mpl.markers.MarkerStyle(marker = "+")
+        if not angle == None: marker_obj._transform = marker_obj.get_transform().rotate_deg(angle)
+        ## self.ax_img.plot(x, y, marker = marker_obj, markersize = 18, color = 'black', markeredgewidth = 4)
+        self.ax_mask.plot(x, y, marker = marker_obj, markersize = 18, color = 'black', markeredgewidth = 4)
+
+
+    def config_colorbar(self, vmin = -1, vcenter = 0, vmax = 1):
+        # Plot image...
+        self.divnorm = mcolors.TwoSlopeNorm(vcenter = vcenter, vmin = vmin, vmax = vmax)
+
+
+    def show(self, center = None, angle = None, title = '', is_save = False): 
+        self.fig, (self.ax_img, self.ax_mask, self.ax_bar_img, self.ax_bar_mask) = self.create_panels()
+
+        self.plot_img()
+        if isinstance(center, tuple): self.plot_center(center, angle)
+        self.plot_mask()
+
+        if isinstance(title, str) and len(title) > 0: plt.suptitle(title)
+
+        if not is_save: 
+            plt.show()
+        else:
+            # Set up drc...
+            DRCPDF         = "pdfs"
+            drc_cwd        = os.getcwd()
+            prefixpath_pdf = os.path.join(drc_cwd, DRCPDF)
+            if not os.path.exists(prefixpath_pdf): os.makedirs(prefixpath_pdf)
+
+            # Specify file...
+            fl_pdf = f"{title}.pdf"
+            path_pdf = os.path.join(prefixpath_pdf, fl_pdf)
+
+            # Export...
+            ## plt.savefig(path_pdf, dpi = 100, bbox_inches='tight', pad_inches = 0)
+            plt.savefig(path_pdf, dpi = 100, transparent=True)
+
+        plt.close('all')
+
+
+
+
+# Config the dataset...
+panels = [0, 1, 2, 3]
+exclude_labels = [ ConfigDataset.UNKNOWN, ConfigDataset.NEEDHELP, ConfigDataset.NOHIT, ConfigDataset.BACKGROUND ]
+config_dataset = ConfigDataset( fl_csv            = 'simulated.pnccd_panel.datasets.csv',
+                                size_sample       = 2000, 
+                                resize            = None,
+                                seed              = 0,
+                                panels            = panels,
+                                isflat            = False,
+                                istrain           = True,
+                                trans             = None,
+                                frac_train        = 0.7,
+                                exclude_labels    = exclude_labels, )
+
+# Preprocess dataset...
+# Data preprocessing can be lengthy and defined in dataset_preprocess.py
+dataset_preproc = DatasetPreprocess(config_dataset)
+dataset_preproc.apply()
+
+# Create image manager...
+with SPIPanelDataset(config_dataset) as spiimg:
+    # Read an image...
+    for idx in range(10):
+        # Don't apply those changes from configuration...
+        spiimg.trans = None
+        img, _ = spiimg[idx]
+        img = img.squeeze(axis = 0)
+
+        fl_base, id_frame, id_panel, label = spiimg.imglabel_list[idx]
+
+        # Apply those changes from configuration...
+        spiimg.trans = dataset_preproc.trans
+
+        # Get transed image...
+        img_masked, _ = spiimg[idx]
+        img_masked = img_masked.squeeze(axis = 0)
+
+        # None rotation...
+        center = None
+        angle  = None
+
+        title = f'panel_simulated.{fl_base}.{id_frame}.{id_panel}.{label}'
+
+        # Normalize image...
+        img_masked = (img_masked - np.mean(img_masked)) / np.std(img_masked)
+
+        # Dispaly an image...
+        ## title = f'imagemask.{idx:06d}'
+        disp_manager = DisplaySPIImg(img, img_masked, figsize = (18, 8))
+        disp_manager.show(center = center, angle = angle, title = title, is_save = False)
+        disp_manager.show(center = center, angle = angle, title = title, is_save = True)
---
+++ examples/train.online.simulated.pnccd_panel.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+import torch
+from deepprojection.datasets.simulated_panels import OnlineDataset      , ConfigDataset
+from deepprojection.model                     import OnlineSiameseModel , ConfigSiameseModel
+from deepprojection.trainer                   import OnlineTrainer      , ConfigTrainer
+from deepprojection.validator                 import OnlineLossValidator, ConfigValidator
+from deepprojection.encoders.convnet          import Hirotaka0122       , ConfigEncoder
+from deepprojection.utils                     import EpochManager       , MetaLog
+from datetime import datetime
+from simulated_pnccd_panel_preprocess import DatasetPreprocess
+import socket
+
+# Set up parameters for an experiment...
+## fl_csv         = "simulated.pnccd_panel.datasets.csv"
+fl_csv         = "simulated.pnccd_panel.v2.datasets.csv"
+size_sample    = 4000
+size_batch     = 40
+alpha          = 2.0
+online_shuffle = True
+lr             = 1e-3
+
+# Clarify the purpose of this experiment...
+hostname = socket.gethostname()
+comments = f"""
+            Hostname: {hostname}.
+
+            Online training.
+
+            Sample size    : {size_sample}
+            Batch  size    : {size_batch}
+            Alpha          : {alpha}
+            Online shuffle : {online_shuffle}
+            lr             : {lr}
+
+            """
+
+# [[[ LOGGING ]]]
+# Create a timestamp to name the log file...
+now = datetime.now()
+timestamp = now.strftime("%Y_%m%d_%H%M_%S")
+
+# Configure the location to run the job...
+drc_cwd = os.getcwd()
+
+# Set up the log file...
+fl_log         = f"{timestamp}.train.log"
+DRCLOG         = "logs"
+prefixpath_log = os.path.join(drc_cwd, DRCLOG)
+if not os.path.exists(prefixpath_log): os.makedirs(prefixpath_log)
+path_log = os.path.join(prefixpath_log, fl_log)
+
+# Config logging behaviors
+logging.basicConfig( filename = path_log,
+                     filemode = 'w',
+                     format="%(asctime)s %(levelname)s %(name)-35s - %(message)s",
+                     datefmt="%m/%d/%Y %H:%M:%S",
+                     level=logging.INFO, )
+logger = logging.getLogger(__name__)
+
+# Create a metalog to the log file, explaining the purpose of this run...
+metalog = MetaLog( comments = comments )
+metalog.report()
+
+
+# [[[ DATASET ]]]
+# Config the dataset...
+exclude_labels = [ ConfigDataset.UNKNOWN, ConfigDataset.NEEDHELP, ConfigDataset.NOHIT, ConfigDataset.BACKGROUND ]
+panels         = [ 0, 1 ,2, 3 ]
+config_dataset = ConfigDataset( fl_csv         = fl_csv,
+                                size_sample    = size_sample, 
+                                resize         = None,
+                                seed           = 0,
+                                panels         = panels,
+                                isflat         = False,
+                                istrain        = True,
+                                frac_train     = 0.7,
+                                trans          = None,
+                                exclude_labels = exclude_labels, )
+
+# Preprocess dataset...
+# Data preprocessing can be lengthy and defined in dataset_preprocess.py
+dataset_preproc = DatasetPreprocess(config_dataset)
+dataset_preproc.apply()
+size_y, size_x = dataset_preproc.get_panelsize()
+
+# Define training set...
+config_dataset.report()
+with OnlineDataset(config_dataset) as dataset_train:
+    # Define validation set...
+    config_dataset.istrain = False
+    config_dataset.report()
+    dataset_validate = OnlineDataset(config_dataset)
+
+
+    # [[[ IMAGE ENCODER ]]]
+    # Config the encoder...
+    dim_emb = 128
+    config_encoder = ConfigEncoder( dim_emb = dim_emb,
+                                    size_y  = size_y,
+                                    size_x  = size_x,
+                                    isbias  = True )
+    encoder = Hirotaka0122(config_encoder)
+
+
+    # [[[ MODEL ]]]
+    # Config the model...
+    config_siamese = ConfigSiameseModel( alpha = alpha, encoder = encoder, )
+    model = OnlineSiameseModel(config_siamese)
+
+    # Initialize weights...
+    def init_weights(module):
+        if isinstance(module, (torch.nn.Embedding, torch.nn.Linear)):
+            module.weight.data.normal_(mean = 0.0, std = 0.02)
+    model.apply(init_weights)
+
+
+    # [[[ CHECKPOINT ]]]
+    DRCCHKPT         = "chkpts"
+    prefixpath_chkpt = os.path.join(drc_cwd, DRCCHKPT)
+    fl_chkpt         = f"{timestamp}.train.chkpt"
+    path_chkpt       = os.path.join(prefixpath_chkpt, fl_chkpt)
+
+
+    # [[[ TRAINER ]]]
+    # Config the trainer...
+    config_train = ConfigTrainer( path_chkpt     = path_chkpt,
+                                  num_workers    = 0,
+                                  batch_size     = size_batch,
+                                  pin_memory     = True,
+                                  shuffle        = False,
+                                  online_shuffle = online_shuffle,
+                                  is_logging     = False,
+                                  method         = 'semi-hard', 
+                                  ## method         = 'random-semi-hard', 
+                                  ## method         = 'random', 
+                                  lr             = lr, )
+
+    # Training...
+    trainer = OnlineTrainer(model, dataset_train, config_train)
+
+
+    # [[[ VALIDATOR ]]]
+    config_validator = ConfigValidator( path_chkpt     = None,
+                                        num_workers    = 0,
+                                        batch_size     = size_batch,
+                                        pin_memory     = True,
+                                        shuffle        = False,
+                                        online_shuffle = online_shuffle,
+                                        is_logging     = False,
+                                        method         = 'semi-hard', 
+                                        ## method         = 'random-semi-hard', 
+                                        ## method         = 'random', 
+                                        lr             = lr, 
+                                        isflat         = False, )  # Conv2d input needs one more dim for batch
+
+    validator = OnlineLossValidator(model, dataset_validate, config_validator)
+
+
+    # [[[ EPOCH MANAGER ]]]
+    max_epochs = 360
+    epoch_manager = EpochManager(trainer = trainer, validator = validator, max_epochs = max_epochs)
+    epoch_manager.run()
