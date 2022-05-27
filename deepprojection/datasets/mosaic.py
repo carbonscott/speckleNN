ad8553c368241456a81475f130fb204b1027b172
carbonscott <wangimagine@gmail.com>
2022-05-26 17:30:34 -0700

    Addressing broken data issue during initialization of dataset loader for mosaic mode.

--- deepprojection/datasets/mosaic.py
+++ deepprojection/datasets/mosaic.py
@@ -13,7 +13,6 @@ import numpy as np
 import random
 import csv
 import os
-import inspect
 
 import logging
 
@@ -87,11 +86,22 @@ class SPIMosaicDataset(Dataset):
                 basename = (exp, run)
 
                 # Initiate image accessing layer...
-                self.psana_imgreader_dict[basename] = PsanaMosaic(exp, run, mode, detector_name)
+                psana_imgreader = PsanaMosaic(exp, run, mode, detector_name)
+                self.psana_imgreader_dict[basename] = psana_imgreader
 
                 # Obtain image labels from this dataset...
                 imglabel_fileparser = ImgLabelFileParser(exp, run, drc_label, exclude_labels)
-                self._dataset_dict[basename] = imglabel_fileparser.imglabel_dict
+
+                # Parse labels in the label file if it exists???
+                if imglabel_fileparser.imglabel_dict:
+                    imglabel_dict = imglabel_fileparser.imglabel_dict
+
+                # Otherwise, fetch all events without label...
+                else:
+                    imglabel_dict = { str(event_num) : "-1" for event_num, _ in enumerate(psana_imgreader.timestamps) 
+                                                            if psana_imgreader.get(event_num, mode = self.mode)[0] is not None }
+
+                self._dataset_dict[basename] = imglabel_dict
 
         # Enumerate each image from all datasets...
         for dataset_id, dataset_content in self._dataset_dict.items():
