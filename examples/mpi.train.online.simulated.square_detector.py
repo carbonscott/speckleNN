f7b6347c4a3d21c8ca757eca9a5e9778d5254994
carbonscott <wangimagine@gmail.com>
2022-07-05 15:14:11 -0700

    Support data loading using MPI (for training).

---
+++ examples/mpi.train.online.simulated.square_detector.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from mpi4py import MPI
+
+import os
+import logging
+import torch
+from deepprojection.datasets.simulated_square_detector import OnlineDataset      , ConfigDataset
+from deepprojection.model                              import OnlineSiameseModel , ConfigSiameseModel
+from deepprojection.trainer                            import OnlineTrainer      , ConfigTrainer
+from deepprojection.validator                          import OnlineLossValidator, ConfigValidator
+from deepprojection.encoders.convnet                   import Hirotaka0122       , ConfigEncoder
+from deepprojection.utils                              import EpochManager       , MetaLog
+from simulated_square_detector_preprocess              import DatasetPreprocess
+from datetime import datetime
+import socket
+
+def chunk_list(input_list, num_chunk = 2):
+
+    chunk_size = len(input_list) // num_chunk + 1
+
+    chunked_list = []
+    for idx_chunk in range(num_chunk):
+        idx_b = idx_chunk * chunk_size
+        idx_e = idx_b + chunk_size
+        if idx_chunk == num_chunk - 1: idx_e = len(input_list)
+        seg   = input_list[idx_b : idx_e]
+        chunked_list.append(seg)
+
+    return chunked_list
+
+
+# ___/ MAIN \___
+# [[[ MPI HEADER ]]]
+# Set up MPI...
+mpi_comm = MPI.COMM_WORLD
+mpi_size = mpi_comm.Get_size()    # num of processors
+mpi_rank = mpi_comm.Get_rank()
+mpi_data_tag  = 11
+
+
+# [[[ PARAMETERS ]]]
+# Set up MPI...
+# Set up parameters for an experiment...
+fl_csv               = "simulated.square_detector.datasets.pdb_sampled.10.csv"
+## fl_csv                = "simulated.square_detector.datasets.6Q5U.csv"
+size_sample_train     = 40000
+size_sample_validate  = 10000
+size_sample_per_class = None
+size_batch            = 1000
+frac_train            = 0.8
+frac_validate         = None
+dataset_usage         = 'train'
+alpha                 = 2.0
+online_shuffle        = True
+lr                    = 1e-3
+seed                  = 0
+
+# Clarify the purpose of this experiment...
+hostname = socket.gethostname()
+comments = f"""
+            Hostname: {hostname}.
+
+            Online training.
+
+            Sample size (train)     : {size_sample_train}
+            Sample size (validate)  : {size_sample_validate}
+            Sample size (per class) : {size_sample_per_class}
+            Batch  size             : {size_batch}
+            Alpha                   : {alpha}
+            Online shuffle          : {online_shuffle}
+            lr                      : {lr}
+
+            """
+
+# [[[ LOGGING ]]]
+# Create a timestamp to name the log file...
+now = datetime.now()
+timestamp = now.strftime("%Y_%m%d_%H%M_%S")
+
+# Configure the location to run the job...
+drc_cwd = os.getcwd()
+
+# Set up the log file...
+fl_log         = f"{timestamp}.train.log"
+DRCLOG         = "logs"
+prefixpath_log = os.path.join(drc_cwd, DRCLOG)
+if not os.path.exists(prefixpath_log): os.makedirs(prefixpath_log)
+path_log = os.path.join(prefixpath_log, fl_log)
+
+# Only log from the main worker...
+if mpi_rank == 0:
+    # Config logging behaviors
+    logging.basicConfig( filename = path_log,
+                         filemode = 'w',
+                         format="%(asctime)s %(levelname)s %(name)-35s - %(message)s",
+                         datefmt="%m/%d/%Y %H:%M:%S",
+                         level=logging.INFO, )
+    logger = logging.getLogger(__name__)
+
+    # Create a metalog to the log file, explaining the purpose of this run...
+    metalog = MetaLog( comments = comments )
+    metalog.report()
+
+
+# [[[ DATASET ]]]
+# Config the dataset...
+exclude_labels = [ ConfigDataset.UNKNOWN, ConfigDataset.NEEDHELP, ConfigDataset.NOHIT, ConfigDataset.BACKGROUND ]
+config_dataset = ConfigDataset( fl_csv                = fl_csv,
+                                size_sample           = size_sample_train, 
+                                seed                  = seed,
+                                isflat                = False,
+                                frac_train            = frac_train,
+                                frac_validate         = frac_validate,
+                                size_sample_per_class = size_sample_per_class,
+                                dataset_usage         = dataset_usage,
+                                trans                 = None,
+                                exclude_labels        = exclude_labels, )
+
+# Define training set...
+dataset_train = OnlineDataset(config_dataset)
+
+# Preprocess dataset...
+# Data preprocessing can be lengthy and defined in dataset_preprocess.py
+img_orig            = dataset_train[0][0][0]
+dataset_preproc     = DatasetPreprocess(img_orig)
+trans               = dataset_preproc.config_trans()
+dataset_train.trans = trans
+img_trans           = dataset_train[0][0][0]
+
+# Split MPI work load (training)...
+train_idx_list         = list(set(dataset_train.online_set))
+train_chunked_idx_list = chunk_list(train_idx_list, mpi_size - 1)
+
+# Load images by each worker...
+if mpi_rank != 0:
+    train_idx_list_by_rank = train_chunked_idx_list[mpi_rank - 1]
+    dataset_train.cache_img(train_idx_list_by_rank)
+
+    mpi_comm.send(dataset_train.imglabel_cache_dict, dest = 0, tag = mpi_data_tag)
+
+if mpi_rank == 0:
+    # Combine data from each worker...
+    for i in range(1, mpi_size, 1):
+        imglabel_cache_dict_by_rank = mpi_comm.recv(source = i, tag = mpi_data_tag)
+        dataset_train.imglabel_cache_dict.update(imglabel_cache_dict_by_rank)
+
+    dataset_train.report()
+
+# Report training set...
+config_dataset.trans = trans
+config_dataset.report()
+
+# Define validation set...
+config_dataset.size_sample           = size_sample_validate
+config_dataset.dataset_usage         = 'validate'
+config_dataset.size_sample_per_class = None
+config_dataset.report()
+dataset_validate = OnlineDataset(config_dataset)
+
+# Split MPI work load (validation)...
+validate_idx_list         = list(set(dataset_validate.online_set))
+validate_chunked_idx_list = chunk_list(validate_idx_list, mpi_size - 1)
+
+# Load images by each worker...
+if mpi_rank != 0:
+    validate_idx_list_by_rank = validate_chunked_idx_list[mpi_rank - 1]
+    dataset_validate.cache_img(validate_idx_list_by_rank)
+
+    mpi_comm.send(dataset_validate.imglabel_cache_dict, dest = 0, tag = mpi_data_tag)
+
+if mpi_rank == 0:
+    # Combine data from each worker...
+    for i in range(1, mpi_size, 1):
+        imglabel_cache_dict_by_rank = mpi_comm.recv(source = i, tag = mpi_data_tag)
+        dataset_validate.imglabel_cache_dict.update(imglabel_cache_dict_by_rank)
+
+    # Finalize MPI...
+    MPI.Finalize()
+
+    # ___/ MODEL TRAINING/VALIDATION \___
+    # [[[ IMAGE ENCODER ]]]
+    # Config the encoder...
+    dim_emb = 128
+    size_y, size_x = img_trans.shape
+    config_encoder = ConfigEncoder( dim_emb = dim_emb,
+                                    size_y  = size_y,
+                                    size_x  = size_x,
+                                    isbias  = True )
+    encoder = Hirotaka0122(config_encoder)
+
+
+    # [[[ MODEL ]]]
+    # Config the model...
+    config_siamese = ConfigSiameseModel( alpha = alpha, encoder = encoder, )
+    model = OnlineSiameseModel(config_siamese)
+
+    # Initialize weights...
+    def init_weights(module):
+        if isinstance(module, (torch.nn.Embedding, torch.nn.Linear)):
+            module.weight.data.normal_(mean = 0.0, std = 0.02)
+    model.apply(init_weights)
+
+
+    # [[[ CHECKPOINT ]]]
+    DRCCHKPT         = "chkpts"
+    prefixpath_chkpt = os.path.join(drc_cwd, DRCCHKPT)
+    fl_chkpt         = f"{timestamp}.train.chkpt"
+    path_chkpt       = os.path.join(prefixpath_chkpt, fl_chkpt)
+
+
+    # [[[ TRAINER ]]]
+    # Config the trainer...
+    config_train = ConfigTrainer( path_chkpt     = path_chkpt,
+                                  num_workers    = 0,
+                                  batch_size     = size_batch,
+                                  pin_memory     = True,
+                                  shuffle        = False,
+                                  online_shuffle = online_shuffle,
+                                  is_logging     = False,
+                                  method         = 'random-semi-hard', 
+                                  lr             = lr, )
+
+    # Training...
+    trainer = OnlineTrainer(model, dataset_train, config_train)
+
+
+    # [[[ VALIDATOR ]]]
+    config_validator = ConfigValidator( path_chkpt     = None,
+                                        num_workers    = 0,
+                                        batch_size     = size_batch,
+                                        pin_memory     = True,
+                                        shuffle        = False,
+                                        online_shuffle = online_shuffle,
+                                        is_logging     = False,
+                                        method         = 'random-semi-hard', 
+                                        lr             = lr, 
+                                        isflat         = False, )  # Conv2d input needs one more dim for batch
+
+    validator = OnlineLossValidator(model, dataset_validate, config_validator)
+
+
+    # [[[ EPOCH MANAGER ]]]
+    max_epochs = 360
+    epoch_manager = EpochManager(trainer = trainer, validator = validator, max_epochs = max_epochs)
+    epoch_manager.run()
