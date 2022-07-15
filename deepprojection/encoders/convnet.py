3097b0d7d28d6711fe475145d0fd0fdbdbadbf3e
carbonscott <wangimagine@gmail.com>
2022-07-14 22:40:01 -0700

    Include a variant model.

--- deepprojection/encoders/convnet.py
+++ deepprojection/encoders/convnet.py
@@ -105,6 +105,112 @@ class Hirotaka0122(nn.Module):
 
 
 
+class Hirotaka0122Plus(nn.Module):
+    """ https://www.kaggle.com/hirotaka0122/triplet-loss-with-pytorch """
+
+    def __init__(self, config):
+        super().__init__()
+
+        size_y, size_x = config.size_y, config.size_x
+        dim_img        = size_y * size_x
+        isbias         = config.isbias
+        dim_emb        = config.dim_emb
+
+        # Define Conv params...
+        # I don't have a good idea of how to make it easier for users.
+        conv_dict = {
+                      "order"  : ("conv 1", "pool 1", "conv 2", "pool 2"),
+                      "conv 1" : { 
+                                   "in_channels"  : 1,
+                                   "out_channels" : 32,
+                                   "kernel_size"  : 3,
+                                   "stride"       : 1,
+                                   "padding"      : 0,
+                                   "bias"         : isbias,
+                                 },
+                      "pool 1" : { 
+                                   "kernel_size" : 2,
+                                   "stride"      : 2,
+                                   "padding"     : 0,
+                                 },
+                      "conv 2" : { 
+                                   "in_channels"  : 32,
+                                   "out_channels" : 64,
+                                   "kernel_size"  : 3,
+                                   "stride"       : 1,
+                                   "padding"      : 0,
+                                   "bias"         : isbias,
+                                 },
+                      "pool 2" : { 
+                                   "kernel_size" : 2,
+                                   "stride"      : 2,
+                                   "padding"     : 0,
+                                 },
+                      ## "conv 3" : { 
+                      ##              "in_channels"  : 64,
+                      ##              "out_channels" : 128,
+                      ##              "kernel_size"  : 5,
+                      ##              "stride"       : 1,
+                      ##              "padding"      : 0,
+                      ##              "bias"         : isbias,
+                      ##            },
+                      ## "pool 3" : { 
+                      ##              "kernel_size" : 2,
+                      ##              "stride"      : 2,
+                      ##              "padding"     : 0,
+                      ##            },
+                    }
+
+        in_channels = 1
+        conv_volume = ConvVolume(size_y, size_x, in_channels, conv_dict)
+        conv_channels, conv_size_y, conv_size_x = conv_volume.shape()
+        self.dim_features = conv_size_y * conv_size_x * conv_channels
+
+        # Define conv object...
+        self.conv = nn.Sequential(
+            # Conv layer for feature extraction...
+            nn.Conv2d(    **conv_dict["conv 1"] ),
+            nn.PReLU(),
+            nn.MaxPool2d( **conv_dict["pool 1"] ),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(    **conv_dict["conv 2"] ),
+            nn.PReLU(),
+            nn.MaxPool2d( **conv_dict["pool 2"] ),
+            nn.Dropout(0.1),
+
+            ## nn.Conv2d(    **conv_dict["conv 3"] ),
+            ## nn.PReLU(),
+            ## nn.MaxPool2d( **conv_dict["pool 3"] ),
+            ## nn.Dropout(0.1),
+        )
+
+        # Define the embedding layer...
+        self.embed = nn.Sequential(
+            nn.Linear( in_features  = self.dim_features, 
+                       out_features = 512, 
+                       bias         = isbias),
+            nn.PReLU(),
+            nn.Linear( in_features  = 512, 
+                       out_features = dim_emb, 
+                       bias         = isbias),
+        )
+
+
+    def encode(self, x):
+        x = self.conv(x)
+        x = x.view(-1, self.dim_features)
+        x = self.embed(x)
+
+        # L2 Normalize...
+        dnorm = torch.norm(x, dim = -1, keepdim = True)
+        x = x / dnorm
+
+        return x
+
+
+
+
 class AdamBielski(nn.Module):
     """ https://github.com/adambielski/siamese-triplet/blob/master/networks.py """
 
