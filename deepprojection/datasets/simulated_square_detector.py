d011bc98c93736174ba82639e243cd4187778a82
carbonscott <wangimagine@gmail.com>
2022-09-01 10:43:28 -0700

    Add support for sequential set.

--- deepprojection/datasets/simulated_square_detector.py
+++ deepprojection/datasets/simulated_square_detector.py
@@ -541,6 +541,47 @@ class SimpleSet(Siamese):
 
 
 
+class SequentialSet(Siamese):
+    """
+    Sequential set feeds one example to model at a time.  The purpose is simply to
+    encode each example in imglabel_list.  
+    """
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        # Force imglabel_list to be the original one...
+        self.imglabel_list = self.imglabel_orig_list
+
+        # Create a lookup table for locating the sequence number (seqi) based on a label...
+        self.label_seqi_orig_dict = {}
+        for seqi, (_, _, label) in enumerate(self.imglabel_orig_list):
+            # Keep track of label and its seqi
+            if not label in self.label_seqi_orig_dict: self.label_seqi_orig_dict[label] = [seqi]
+            else                                     : self.label_seqi_orig_dict[label].append(seqi)
+
+        return None
+
+
+    def __len__(self):
+        return len(self.imglabel_orig_list)
+
+
+    def __getitem__(self, idx):
+        # Read the single image...
+        img, _ = super().__getitem__(idx)
+        res = (img, )
+
+        # Append metadata to the result list...
+        fl_base, id_frame, label = self.imglabel_orig_list[idx]
+        title = f"{fl_base} {id_frame:>06d} {label}"
+        res += (title, )
+
+        return res
+
+
+
+
 class OnlineDataset(Siamese):
     """
     For online leraning.  
