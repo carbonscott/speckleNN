67bc745774917a071ae6b9fa0f74d95b19e9b894
carbonscott <wangimagine@gmail.com>
2023-02-23 16:03:55 -0800

    Try if I can remove dependency on psana.

--- speckleNN/__init__.py
+++ speckleNN/__init__.py
@@ -1,8 +1,8 @@
-from . import model, trainer, utils, validator
-
-__all__ = [
-            "model", 
-            "trainer", 
-            "uitls", 
-            "validator", 
-]
+## from . import model, trainer, utils, validator
+## 
+## __all__ = [
+##             "model", 
+##             "trainer", 
+##             "uitls", 
+##             "validator", 
+## ]
--- speckleNN/datasets/__init__.py
+++ speckleNN/datasets/__init__.py
@@ -1,12 +1,12 @@
-from . import simulated_panels, simulated_square_detector, panels, mosaic, images, transform, fastdata, lite
-
-__all__ = [
-    "simulated_panels",
-    "simulated_square_detector",
-    "panels",
-    "mosaic",
-    "images",
-    "transform",
-    "fastdata",
-    "lite",
-]
+## from . import simulated_panels, simulated_square_detector, panels, mosaic, images, transform, fastdata, lite
+## 
+## __all__ = [
+##     "simulated_panels",
+##     "simulated_square_detector",
+##     "panels",
+##     "mosaic",
+##     "images",
+##     "transform",
+##     "fastdata",
+##     "lite",
+## ]
